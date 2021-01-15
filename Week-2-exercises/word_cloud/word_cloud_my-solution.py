1b0f7366966f5b1aa6074cb9ec056edd18901442
Jose Reyes <jaeyow@gmail.com>
2021-01-15 20:58:15 +1100

    week 2 exercises

---
+++ Week-2-exercises/word_cloud/word_cloud_my-solution.py
@@ -0,0 +1,41 @@
+import collections
+
+# open file resources
+input_file=open('98-0.txt', encoding="utf8")
+stopword_file=open('stopwords.txt', encoding="utf8")
+
+num_words = 10
+
+# prepare stopwords table
+stopwords=set(stopword_file)
+stopwords_list = list(val \
+    .strip('?; ()-#_.,\"\n\'') \
+    .lower() \
+    .replace("'", "") \
+    .replace("!", "") \
+    .replace("-", "") \
+    .replace("?", "") \
+    for val in stopwords)
+
+# prepare sanitised key
+wordcount={}
+for key in input_file.read().split():
+    sanitised_key = key \
+        .strip('?; ()-#_.,\"\n\'') \
+        .lower() \
+        .replace("'", "") \
+        .replace("!", "") \
+        .replace("?", "") \
+        .replace("-", "")
+        
+    # conditionally populate dictionary
+    if sanitised_key not in stopwords_list:
+        if sanitised_key in wordcount:
+            wordcount[sanitised_key] = wordcount[sanitised_key] + 1
+        else:
+            wordcount[sanitised_key] = 1
+ 
+# print summary
+print('Top 10 Wordcount: ')
+for w in sorted(wordcount, key=wordcount.get, reverse=True)[:num_words]:
+    print(w, wordcount[w])
